0ece1dc00441033d1bc5dbdcc72b909bf57dc45c
chloe-661 <chloesunter@gmail.com>
2021-12-02 02:40:37 +0000

    Auth Controller & Routes added

--- DigitalFridge/server.js
+++ DigitalFridge/server.js
@@ -4,8 +4,12 @@ const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
 
+const authRoutes = require('./routes/auth');
+
 const app = express();
 app.use(express.json());
+
+app.use('/api',authRoutes);
 
 // used in production to serve client files
 if(process.env.NODE_ENV === 'production') {
@@ -18,7 +22,6 @@ if(process.env.NODE_ENV === 'production') {
 // connecting to mongoDB and then running server on port 4000
 const dbURI = config.get('dbURI');
 const port = process.env.PORT || 4000;
-
 
 mongoose.connect(dbURI)
   .then((result) => {
