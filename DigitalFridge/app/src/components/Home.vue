6659142af3757dd7c8877fa999469f873b1613e4
chloe-661 <chloesunter@gmail.com>
2021-12-03 02:01:55 +0000

    Updated Home Component Name

--- DigitalFridge/app/src/components/Home.vue
+++ DigitalFridge/app/src/components/Home.vue
@@ -2,7 +2,7 @@
 <div>
   <img src="../assets/Note.png"/>
   <h1> Digital Fridge </h1>
-  <h4> Store notes, checklists, photos and more all in one place </h4>
+  <h4> Store notes, checklists, photos and more all in one place! </h4>
   <h4> Convert handwriting to text </h4>
 </div>
 </template>
