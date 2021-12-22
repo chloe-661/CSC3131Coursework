fbe09298636540f6a87ff3b1dd74716c6d79f7df
chloe-661 <chloesunter@gmail.com>
2021-12-22 05:26:42 +0000

    update

---
+++ DigitalFridge/README.md
@@ -0,0 +1,15 @@
+To run locally:
+
+Backend - port 3000
+---------------------
+in ----> ./DigitalFridge
+run:
+    npm install
+    nodemon
+
+Front end - port 8080
+---------------------
+in ----> ./DigitalFridge/client/app
+run:
+    npm install
+    npm run serve
---
+++ DigitalFridge/client/app/src/components/loadingSpinner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="loading-spinner"></div>
+</template>
+
+<style scoped>
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.loading-spinner {
+  transition: 0.3s ease all;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+
+.loading-spinner::after {
+  content: '';
+  display: inline-block;
+  transition: 0.3s ease all;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  width: inherit;
+  height: inherit;
+  border: 2px solid #138e96;
+  border-radius: 50%;
+  border-top-color: transparent;
+  animation: spin 1s ease-in-out infinite;
+  left: 0;
+  width: 80%;
+  height: 80%;
+}
+</style>
